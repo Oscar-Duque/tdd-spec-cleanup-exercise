8147647b61706e9e8189087079dbcc0c31bcfd78
oscar duque <dukescar@gmail.com>
2021-11-26 20:15:09 -0600

    refact: invitation_spec.rb

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,37 +1,48 @@
 require "rails_helper"
 
 RSpec.describe Invitation do
-  let(:invitation) { build(:invitation, team: team, user: new_user) }
-  let(:new_user) { create(:user, email: "rookie@example.com") }
-  let(:team) { create(:team, name: "A fine team") }
-  let(:team_owner) { create(:user) }
-
-  before do
+  def generate_full_team
+    team = create(:team, name: "A fine team")
+    team_owner = create(:user)
     team.update!(owner: team_owner)
     team_owner.update!(team: team)
+
+    team
   end
 
   describe "callbacks" do
     describe "after_save" do
       context "with valid data" do
         it "invites the user" do
+          team = generate_full_team
+          new_user = create(:user)
+          invitation = build(:invitation, team: team, user: new_user)
+
           invitation.save
+
           expect(new_user).to be_invited
         end
       end
 
       context "with invalid data" do
-        before do
-          invitation.team = nil
-          invitation.save
-        end
-
         it "does not save the invitation" do
+          team = nil
+          new_user = create(:user)
+          invitation = build(:invitation, team: team, user: new_user)
+
+          invitation.save
+
           expect(invitation).not_to be_valid
           expect(invitation).to be_new_record
         end
 
         it "does not mark the user as invited" do
+          team = nil
+          new_user = create(:user)
+          invitation = build(:invitation, team: team, user: new_user)
+
+          invitation.save
+
           expect(new_user).not_to be_invited
         end
       end
@@ -40,42 +51,69 @@ RSpec.describe Invitation do
 
   describe "#event_log_statement" do
     context "when the record is saved" do
-      before do
-        invitation.save
-      end
-
       it "include the name of the team" do
+        team = generate_full_team
+        new_user = create(:user)
+        invitation = build(:invitation, team: team, user: new_user)
+
+        invitation.save
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("A fine team")
       end
 
       it "include the email of the invitee" do
+        team = generate_full_team
+        new_user = create(:user, email: "rookie@example.com")
+        invitation = build(:invitation, team: team, user: new_user)
+
+        invitation.save
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("rookie@example.com")
       end
     end
 
     context "when the record is not saved but valid" do
       it "includes the name of the team" do
+        team = generate_full_team
+        new_user = create(:user)
+        invitation = build(:invitation, team: team, user: new_user)
+
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("A fine team")
       end
 
       it "includes the email of the invitee" do
+        team = generate_full_team
+        new_user = create(:user, email: "rookie@example.com")
+        invitation = build(:invitation, team: team, user: new_user)
+
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("rookie@example.com")
       end
 
       it "includes the word 'PENDING'" do
+        team = generate_full_team
+        new_user = create(:user)
+        invitation = build(:invitation, team: team, user: new_user)
+
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("PENDING")
       end
     end
 
     context "when the record is not saved and not valid" do
       it "includes INVALID" do
-        invitation.user = nil
+        team = generate_full_team
+        new_user = nil
+        invitation = build(:invitation, team: team, user: new_user)
+
         log_statement = invitation.event_log_statement
+
         expect(log_statement).to include("INVALID")
       end
     end
